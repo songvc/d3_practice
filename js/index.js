b0d80a90c222b1d8a6158ab8776b73383100ccac
Vincent Song <songvc@users.noreply.github.com>
2015-12-09 16:42:37 -0800

    first commit

---
+++ js/index.js
@@ -0,0 +1,18 @@
+
+var data = [4, 8, 15, 16, 23, 42];
+
+var x = d3.scale.linear()
+    .domain([0, d3.max(data)])
+    .range([0, 200]);
+
+d3.select('.chart')
+	.selectAll("div")
+		.data(data)
+	.enter().append("div")
+		.attr("class", "bar")
+		.style("width", function(d){ return x(d) + "px";})
+		.text(function(d){ return d; });
+
+
+
+	
